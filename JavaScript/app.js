8747b2277750d5025839f065b8f0011216275c7a
Maria <m48173550@gmail.com>
2025-12-13 16:00:32 -0300

    CSS e JS

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,9 +1,25 @@
-window.addEventListener("scroll", function () {
+window.addEventListener("scroll", () => {
     const header = document.querySelector("header");
 
-    if (window.scrollY > 10) {
+    if (window.scrollY > 80) {
         header.classList.add("scrolled");
     } else {
         header.classList.remove("scrolled");
     }
+});
+document.addEventListener("DOMContentLoaded", () => {
+    const textoSobre = document.querySelector("#sobre-escola p");
+
+    const observer = new IntersectionObserver(
+        ([entry]) => {
+            if (entry.isIntersecting) {
+                textoSobre.classList.add("show");
+            }
+        },
+        {
+            threshold: 0.3
+        }
+    );
+
+    observer.observe(textoSobre);
 });
